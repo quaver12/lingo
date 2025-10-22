c40c5b8d7c07f82c0fe34ae383a0ffe35b91b774
Marcus Branney <mbranney12@icloud.com>
2025-10-22 15:05:33 +0100

    Main() takes arguments e.g. 'add' and executes functions depending on choice.

--- lingome.c
+++ lingome.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+void hitme(){
+    printf("hitme()\n");
+}
+
+void add(){
+    printf("add()\n");
+}
+
+void verb(){
+    printf("verb()\n");
+}
+
+void noun(){
+    printf("noun()\n");
+}
 
 
+
+
+
+int main(int argc, char *argv[]){
+
+    if (argc == 1){
+        hitme();
+
+    } else if (strcmp(argv[1],"add")==0){
+        add();
+
+    } else if (strcmp(argv[1],"verb")==0){
+        verb();
+
+    } else if (strcmp(argv[1],"noun")==0){
+        noun();
+    }
 
 
     return 0;
