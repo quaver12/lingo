c4ed36d90bbbe8066b2bb48e53ea7c66d8dbfa3f
Marcus Branney <mbranney12@icloud.com>
2025-10-24 18:56:59 +0100

    Addn succesfully takes input of words and writes onto file e.g adds 'der Hund' from executing ./lingome addn der Hund.

--- lingome.c
+++ lingome.c
@@ -9,13 +9,21 @@ void addv(){
     printf("addv()\n");
 }
 
-void addn(){
+void addn(char *inputs[], int numOfInputs){
+
     printf("addn\n");
 
     FILE *lanlib;
-    lanlib = fopen("nounlibrary.txt","w");
+    lanlib = fopen("nounlibrary.txt","a");
+    
+    fprintf(lanlib, "\n");
+    for (int i = 0; i<numOfInputs;i++){
+        fprintf(lanlib, inputs[i]);
+        fprintf(lanlib, ", ");
+    }
 
-    fprintf(lanlib, "This is a noun");
+
+    
 
 
 
@@ -36,6 +44,12 @@ void noun(){
 
 int main(int argc, char *argv[]){
 
+    //for (int i=0;i<argc;i++){
+    //    printf("%c",*argv[i]);
+//
+  //  }
+
+
     if (argc == 1){
         hitme();
 
@@ -43,7 +57,18 @@ int main(int argc, char *argv[]){
         addv();
 
     } else if (strcmp(argv[1],"addn")==0){
-        addn();
+
+        char *input[10];
+
+        // shift argc left 2 so it is only the words to be added to file
+        // save that as 'input'
+        for (int i = 0; i < argc-2; i++){
+            // sets pointers in input array to the location of relevant pointers in argv
+            input[i] = argv[i+2];
+        }
+
+        //printf("%c",*input[1]);
+        addn(input, argc-2);
 
     } else if (strcmp(argv[1],"verb")==0){
         verb();
